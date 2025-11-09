def44f34a453559c72855c13e50d801a098f5db1
Anurag Gulati <anuraggulati@Anurags-MacBook-Air.local>
2025-11-09 14:13:30 +0530

    Save user to DB in case of successful login

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,13 +2,24 @@ import httpx
 import json
 import time
 import logging
+from datetime import datetime, timezone
 from httpx import HTTPStatusError
 from typing import Dict, Any
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from commons.utils.dependencies import get_secrets_manager  # type: ignore
+from commons.utils.dependencies import get_secrets_manager, get_database_service  # type: ignore
 from .utils.router import Router
 from .config.constants import AUTH_ROUTER_PREFIX
-from .config.settings import CLIENT_ID, REDIRECT_URI, AWS_REGION_NAME, SECRET_GITHUB_OAUTH
+from .config.settings import (
+    CLIENT_ID, 
+    REDIRECT_URI, 
+    AWS_REGION_NAME,
+    SECRET_GITHUB_OAUTH, 
+    SECRET_DATABASE_USERNAME_PATH, 
+    SECRET_DATABASE_PASSWORD_PATH,
+    DATABASE_CONNECTION_STRING,
+    DATABASE_NAME,
+    USERS_COLLECTION
+)
 
 
 router = Router()
@@ -33,15 +44,26 @@ def github_oauth(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any
 @router.route("GET", f"{AUTH_ROUTER_PREFIX}/oauth/github/callback")
 def github_oauth_callback(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
     code = event.get("queryStringParameters", {}).get("code", "")
-    with httpx.Client() as client:
+    github_client_secret, database_username, database_password = tuple(
+        get_secrets_manager(AWS_REGION_NAME).get_secrets([  # type: ignore
+            SECRET_GITHUB_OAUTH, SECRET_DATABASE_USERNAME_PATH, SECRET_DATABASE_PASSWORD_PATH
+        ])
+    )
+    with httpx.Client() as http_client, \
+        get_database_service(
+            conn_string=DATABASE_CONNECTION_STRING,
+            database_name=DATABASE_NAME,
+            username=database_username,
+            password=database_password
+        ) as db_client:
         logger.info("Fetching access token")
         start = time.time()
-        token_response = client.post(
+        token_response = http_client.post(
             url="https://github.com/login/oauth/access_token",
             headers={"Accept": "application/json"},
             data={
                 "client_id": CLIENT_ID,
-                "client_secret": get_secrets_manager(AWS_REGION_NAME).get_secret(SECRET_GITHUB_OAUTH),
+                "client_secret": github_client_secret,
                 "code": code,
                 "redirect_uri": REDIRECT_URI,
             }
@@ -53,7 +75,10 @@ def github_oauth_callback(event: Dict[str, Any], context: LambdaContext) -> Dict
             logger.error("Error fetching access token: %s", str(err))
             return {
                 "statusCode": 400,
-                "body": str(err)
+                "body": json.dumps({
+                    "login_status": "FAILED",
+                    "error": str(err)
+                })
             }
         
         token_data: Dict[str, Any] = token_response.json()
@@ -61,7 +86,7 @@ def github_oauth_callback(event: Dict[str, Any], context: LambdaContext) -> Dict
 
         logger.info("Fetching user data")
         start = time.time()
-        user_response = client.get(
+        user_response = http_client.get(
             url="https://api.github.com/user",
             headers={
                 "Authorization": f"Bearer {access_token}",
@@ -76,13 +101,27 @@ def github_oauth_callback(event: Dict[str, Any], context: LambdaContext) -> Dict
             logger.error("Error fetching user data: %s", str(err))
             return {
                 "statusCode": 500,
-                "body": str(err)
+                "body": json.dumps({
+                    "login_status": "FAILED",
+                    "error": str(err)
+                })
             }
             
-        user_data = user_response.json()
+        user_data: Dict[str, Any] = user_response.json()
+        db_client.save(
+            collection=USERS_COLLECTION,
+            data={
+                **user_data,
+                "access_token": access_token,
+                "login_ts": datetime.now(tz=timezone.utc)
+            }
+        )
+
         return {
             "statusCode": 200,
-            "body": json.dumps(user_data)
+            "body": json.dumps({
+                "login_status": "OK"
+            })
         }
 
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
